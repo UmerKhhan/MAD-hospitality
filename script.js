279e432b9beaa19f9714f0ecd8f27cc66b92e66e
Umer khan <umerg8225@gmail.com>
2025-09-18 19:46:38 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelector(".nav-bar .show_more.About")
+  .addEventListener("mouseenter", () => {
+    document.querySelector(".nav-bar .more_content.About").classList.add("ON");
+  });
+document.querySelector(".nav-bar .more_content.About")
+  .addEventListener("mouseleave", () => {
+    document.querySelector(".nav-bar .more_content.About").classList.remove("ON");
+  });
+
+  document.querySelector(".mb-nav-bar .show_more.About")
+  .addEventListener("mouseenter", () => {
+    document.querySelector(".mb-nav-bar .more_content.About").classList.add("ON");
+  });
+document.querySelector(".mb-nav-bar .more_content.About")
+  .addEventListener("mouseleave", () => {
+    document.querySelector(".mb-nav-bar .more_content.About").classList.remove("ON");
+  });
