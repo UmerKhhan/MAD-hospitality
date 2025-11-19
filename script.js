12cd3155ec548f3753f56507d2e933abcfebfcc8
Umer Khhan <umerg8225@gmail.com>
2025-11-19 13:52:53 +0500

    "Assets and pages upload"

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
+let showContent_about = document.querySelector(".nav-bar .show_more.About");
+let moreContent_about =document.querySelector(".nav-bar .more_content.About");
+
+
 document.querySelector(".nav-bar .show_more.About")
   .addEventListener("mouseenter", () => {
     document.querySelector(".nav-bar .more_content.About").classList.add("ON");
   });
-document.querySelector(".nav-bar .more_content.About")
+document
+  .querySelector(".nav-bar .more_content.About")
   .addEventListener("mouseleave", () => {
-    document.querySelector(".nav-bar .more_content.About").classList.remove("ON");
+    document
+      .querySelector(".nav-bar .more_content.About")
+      .classList.remove("ON");
   });
 
-  document.querySelector(".mb-nav-bar .show_more.About")
+document
+  .querySelector(".mb-nav-bar .show_more.About")
   .addEventListener("mouseenter", () => {
-    document.querySelector(".mb-nav-bar .more_content.About").classList.add("ON");
+    document
+      .querySelector(".mb-nav-bar .more_content.About")
+      .classList.add("ON");
   });
-document.querySelector(".mb-nav-bar .more_content.About")
+document
+  .querySelector(".mb-nav-bar .more_content.About")
   .addEventListener("mouseleave", () => {
-    document.querySelector(".mb-nav-bar .more_content.About").classList.remove("ON");
+    document
+      .querySelector(".mb-nav-bar .more_content.About")
+      .classList.remove("ON");
   });
